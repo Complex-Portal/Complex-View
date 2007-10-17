c11bb0a567575404cc603e7660cb02549c9f1b60
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-10-17 11:19:50 +0000

    IAS-57 (In Binary search results table, hightlight elements which from the same experiments on mouse over)

--- search/binary/binary-search-webapp/src/main/webapp/js/intact.js
+++ search/binary/binary-search-webapp/src/main/webapp/js/intact.js
@@ -1,16 +1,57 @@
-function submitEnter(commandId,e)
+function submitEnter(commandId, e)
 {
-        var keycode;
-        if (window.event)
-                keycode = window.event.keyCode;
-        else if (e)
-                keycode = e.which;
-        else
-                return true;
+    var keycode;
+    if (window.event)
+        keycode = window.event.keyCode;
+    else if (e)
+        keycode = e.which;
+    else
+        return true;
 
-        if (keycode == 13) {
-                document.getElementById(commandId).click();
-                return false;
-        } else
-                return true;
+    if (keycode == 13) {
+        document.getElementById(commandId).click();
+        return false;
+    } else
+        return true;
+}
+
+function startsWith(string, prefix) {
+    return (string.substr(0, prefix.length) == prefix);
+}
+
+function endsWith(string, suffix) {
+    return (string.substr(string.length - suffix.length, string.length) == suffix);
+}
+
+/**
+ * Changes the color for the tags in the document with have an id that starts with the idPrefix
+ */
+function hilightElements(idPrefix, idSuffix, tag, color)
+{
+    var objs = document.getElementsByTagName(tag);
+
+    for (var i = 0; i < objs.length; i++) {
+
+        if (startsWith(objs[i].id, idPrefix))
+        {
+            if (endsWith(objs[i].id, idSuffix)) {
+                document.getElementById(objs[i].id).style.color = color;
+            //} else {
+            //    document.getElementById(objs[i].id).style.color = 'inherit';
+            }
+        }
+    }
+}
+
+function stopHilighting(idPrefix, tag)
+{
+    var objs = document.getElementsByTagName(tag);
+
+    for (var i = 0; i < objs.length; i++) {
+
+        if (startsWith(objs[i].id, idPrefix))
+        {
+           document.getElementById(objs[i].id).style.color = 'inherit';
+        }
+    }
 }
