270f35e5ea37c53cfefa411d2fbd72f128ca7e1b
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-12 11:55:25 +0000

    Attempt to make the perl client work

--- search/binary/binary-webservice/wsclient/src/perl/clientdemo.pl
+++ search/binary/binary-webservice/wsclient/src/perl/clientdemo.pl
@@ -2,25 +2,27 @@
 # SOAP::Lite version 0.67
 # Please note: IntAct webservices uses document/literal binding
 
-use SOAP::Lite + trace => qw(debug);
-#use SOAP::Lite;
+#use SOAP::Lite + trace => qw(debug);
+use SOAP::Lite;
 
 # Setup service
-my $WSDL = 'http://localhost:9090/intact/ws-1.6.0-beta-2/binarysearch?wsdl';
-#my $WSDL = 'http://www.ebi.ac.uk/intact/binarysearch-ws/binarysearch?wsdl';
+#my $WSDL = 'http://localhost:9090/intact/ws-1.6.0-beta-2/binarysearch?wsdl';
+my $WSDL = 'http://www.ebi.ac.uk/intact/binary-search-ws/binarysearch?wsdl';
 my $nameSpace = 'http://ebi.ac.uk/intact/binarysearch/wsclient/generated';
 my $soap = SOAP::Lite
 -> uri($nameSpace)
 -> proxy($WSDL);
 
 # Setup method and parameters
-    my $method = SOAP::Data->name('findBinaryInteractions')
-    ->attr({xmlns => $nameSpace});
+    #my $method = SOAP::Data->name('findBinaryInteractions')
+    #->attr({xmlns => $nameSpace});
 
-    my @params = ( SOAP::Data->name(query => 'P12345'));
+    #my @params = ( SOAP::Data->name('query' => 'P12345') ->attr({xmlns => $nameSpace}) );
 
     # Call method
-    my $result = $soap->call($method => @params);
+    #my $result = $soap->call($method => @params);
+
+    my $result = $soap->findBinaryInteractions('P12345');
 
 # if no error
 unless ($result->fault) {
