6379a78da057beb7980e113d494cca2e507864d5
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-11-12 15:19:02 +0000

    Working perl script

--- search/binary/binary-webservice/wsclient/src/perl/clientdemo.pl
+++ search/binary/binary-webservice/wsclient/src/perl/clientdemo.pl
@@ -5,31 +5,36 @@
 #use SOAP::Lite + trace => qw(debug);
 use SOAP::Lite;
 
+
+# Query
+my $myQuery = 'brca2';
+
 # Setup service
-#my $WSDL = 'http://localhost:9090/intact/ws-1.6.0-beta-2/binarysearch?wsdl';
+#my $WSDL = 'http://localhost:7877/intact/ws/binarysearch?wsdl';
 my $WSDL = 'http://www.ebi.ac.uk/intact/binary-search-ws/binarysearch?wsdl';
 my $nameSpace = 'http://ebi.ac.uk/intact/binarysearch/wsclient/generated';
+
 my $soap = SOAP::Lite
 -> uri($nameSpace)
 -> proxy($WSDL);
 
-# Setup method and parameters
-    #my $method = SOAP::Data->name('findBinaryInteractions')
-    #->attr({xmlns => $nameSpace});
 
-    #my @params = ( SOAP::Data->name('query' => 'P12345') ->attr({xmlns => $nameSpace}) );
+# Setup method and parameters
+    my $method = SOAP::Data->name('findBinaryInteractions') 
+                           ->attr({xmlns => $nameSpace});
+
+    my @params = ( SOAP::Data->name(query => $myQuery));
 
     # Call method
-    #my $result = $soap->call($method => @params);
+    my $result = $soap->call($method => @params);
 
-    my $result = $soap->findBinaryInteractions('P12345');
 
 # if no error
 unless ($result->fault) {
 
     # Retrieve for example all ChEBI identifiers for the ontology parents
-    #@stuff = $result->valueof('//OntologyParents//chebiId');
-    #print @stuff;
+    @stuff = $result->valueof('//interactionLines');
+    print @stuff;
 
 } else {
    # some error handling
