dbe80e973f29b15abd2c90cf22ad9aed30a7f3d5
nneuhaus_ebi <nneuhaus_ebi@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-12-20 16:42:52 +0000

    [IAV-14] Include new Tabs into HierarchView

--- hierarch-view/legacy/hierarch-view-app/src/main/java/uk/ac/ebi/intact/application/hierarchview/business/graph/EdgeAttributes.java
+++ hierarch-view/legacy/hierarch-view-app/src/main/java/uk/ac/ebi/intact/application/hierarchview/business/graph/EdgeAttributes.java
@@ -19,7 +19,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import uk.ac.ebi.intact.application.hierarchview.business.IntactUserI;
 import uk.ac.ebi.intact.application.hierarchview.business.image.Utilities;
-import uk.ac.ebi.intact.service.graph.Node;
+import uk.ac.ebi.intact.service.graph.Edge;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -27,14 +27,13 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * Contains the Attributes of a Node.
+ * TODO comment that class header
  *
  * @author Nadin Neuhauser
  * @version $Id$
  * @since 1.6.0-Snapshot
  */
-public class NodeAttributes {
-
+public class EdgeAttributes {
     private static final Log logger = LogFactory.getLog( NodeAttributes.class );
 
     //private final Node node;
@@ -43,34 +42,29 @@ public class NodeAttributes {
      * *********************************************************************
      * StrutsConstants
      */
-    public final static Color DEFAULT_NODE_COLOR = new Color( 0, 0, 255 );
-    public final static Color DEFAULT_LABEL_COLOR = new Color( 255, 255, 255 );
+    public final static Color DEFAULT_EDGE_COLOR = new Color( 75, 158, 179 );
 
-    public final static Color NODE_COLOR;
-    public final static Color LABEL_COLOR;
+    public final static Color EDGE_COLOR;
 
     static {
         Properties properties = IntactUserI.GRAPH_PROPERTIES;
         String stringColorNode = null;
-        String stringColorLabel = null;
+
         // it is tried to parse the colors from the properties file
         if ( null != properties ) {
-            stringColorNode = properties.getProperty( "hierarchView.image.color.default.node" );
-            stringColorLabel = properties.getProperty( "hierarchView.image.color.default.label" );
+            stringColorNode = properties.getProperty( "hierarchView.image.color.default.edge" );
         } else {
             logger.warn( "properties file GRAPH_PROPERTIES could not been read" );
         }
-        // the color for the node is parsed
-        NODE_COLOR = Utilities.parseColor( stringColorNode, DEFAULT_NODE_COLOR );
-        // the color for the lables parsed
-        LABEL_COLOR = Utilities.parseColor( stringColorLabel, DEFAULT_LABEL_COLOR );
+        // the color for the edge is parsed
+        EDGE_COLOR = Utilities.parseColor( stringColorNode, DEFAULT_EDGE_COLOR );
     }
 
     private Map<String, Object> attributes;
 
 
-    public NodeAttributes( Node node ) {
-        //this.node = node;
+    public EdgeAttributes( Edge edge ) {
+        //this.edge = edge;
         attributes = new HashMap();
     }
 
@@ -80,5 +74,16 @@ public class NodeAttributes {
 
     public Object get( String key ) {
         return attributes.get( key );
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer attribute = new StringBuffer();
+        attribute.append( "EdgeAttributes { " );
+        for ( String key : attributes.keySet() ) {
+            attribute.append( key ).append( " : " ).append( attributes.get( key ) ).append( "," );
+        }
+        attribute.append( " }" );
+        return attribute.toString();
     }
 }
