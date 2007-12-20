f73c92ab3225cf9c20c03ccc95612477405f601e
nneuhaus_ebi <nneuhaus_ebi@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2007-12-20 16:40:36 +0000

    [IAV-15] HighlightInteractions -> Highlight of Edges

--- hierarch-view/legacy/hierarch-view-app/src/main/java/uk/ac/ebi/intact/application/hierarchview/highlightment/HighlightInteractions.java
+++ hierarch-view/legacy/hierarch-view-app/src/main/java/uk/ac/ebi/intact/application/hierarchview/highlightment/HighlightInteractions.java
@@ -1,9 +1,18 @@
-/*
- * Copyright (c) 2002 The European Bioinformatics Institute, and others. All
- * rights reserved. Please see the file LICENSE in the root directory of this
- * distribution.
+/**
+ * Copyright 2007 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package uk.ac.ebi.intact.application.hierarchview.highlightment;
 
 import org.apache.commons.logging.Log;
@@ -14,50 +23,45 @@ import uk.ac.ebi.intact.application.hierarchview.business.graph.Network;
 import uk.ac.ebi.intact.application.hierarchview.business.image.DrawGraph;
 import uk.ac.ebi.intact.application.hierarchview.business.image.ImageBean;
 import uk.ac.ebi.intact.application.hierarchview.highlightment.behaviour.HighlightmentBehaviour;
-import uk.ac.ebi.intact.application.hierarchview.highlightment.source.node.NodeHighlightmentSource;
+import uk.ac.ebi.intact.application.hierarchview.highlightment.source.edge.EdgeHighlightmentSource;
 import uk.ac.ebi.intact.context.IntactContext;
-import uk.ac.ebi.intact.service.graph.Node;
+import uk.ac.ebi.intact.service.graph.Edge;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collection;
 
 /**
- * Allows to perfoem the highlightment according to data stored in the user
- * session
+ * TODO comment that class header
  *
- * @author Samuel Kerrien (skerrien@ebi.ac.uk) & Alexandre Liban (aliban@ebi.ac.uk)
+ * @author Nadin Neuhauser
  * @version $Id$
+ * @since 1.6.0-Snapshot
  */
+public class HighlightInteractions {
 
-public class HighlightProteins {
+    private static final Log logger = LogFactory.getLog( HighlightInteractions.class );
 
-    private static final Log logger = LogFactory.getLog( HighlightProteins.class );
-
-
-    public static void perform( NodeHighlightmentSource nodeHighlightmentSource, String behaviourClass, HttpSession session, Network in ) {
+    public static void perform( EdgeHighlightmentSource edgeHighlightmentSource, String behaviourClass, HttpSession session, Network in ) {
         /*
-         * Put the default color and default visibility in the interaction
-         * network before to highlight this one.
+         * Put the default color and default visibility in the interaction network before to highlight this one.
          */
-        in.initNodes();
         in.initEdges();
+        in.initNodes();
 
-        // Search the protein to highlight
-        Collection<Node> proteinsToHighlight = nodeHighlightmentSource.proteinToHightlight( session, in );
+        // Search the interactions to highlight
+        Collection<Edge> interactionsToHighlight = edgeHighlightmentSource.interactionToHightlight( session, in );
 
-        // Check if the protein selected is in the selected tab
+        // Check if the interaction selected is in the selected tab
         IntactUserI user = ( IntactUserI ) IntactContext.getCurrentInstance().getSession().getAttribute( Constants.USER_KEY );
 
         // Interaction network's modification
-        HighlightmentBehaviour highlightmentBehaviour;
-        highlightmentBehaviour = HighlightmentBehaviour.getHighlightmentBehaviour( behaviourClass );
+        HighlightmentBehaviour highlightmentBehaviour = HighlightmentBehaviour.getHighlightmentBehaviour( behaviourClass );
 
         // apply the highlight to the selected set of protein
-        logger.info( "Proteins collection to be highlighted : " + proteinsToHighlight );
-        highlightmentBehaviour.apply( proteinsToHighlight, in );
+        logger.info( "Interactions collection to be highlighted : " + interactionsToHighlight );
+        highlightmentBehaviour.apply( interactionsToHighlight, in );
 
         // store data in the session
-        // IntactUserI user = (IntactUserI) session.get( Constants.USER_KEY );
         if ( user == null ) {
             logger.info( "USER is null, exit the highlight process" );
             return;
@@ -77,12 +81,10 @@ public class HighlightProteins {
 
         // TODO: needed ?! have to be tested !
         user.setInteractionNetwork( in );
-
     }
 
     /**
-     * Constructor Allow to modify the current graph to highlight a part of
-     * this.
+     * Constructor Allow to modify the current graph to highlight a part of this.
      *
      * @param source         The highlighting source
      * @param behaviourClass The highlighting behaviour class name
@@ -92,8 +94,8 @@ public class HighlightProteins {
     public static void perform( String source, String behaviourClass, HttpSession session, Network in ) {
 
         // Search the highlight source implementation
-        NodeHighlightmentSource nodeHighlightmentSource = NodeHighlightmentSource.getHighlightmentSource( source );
+        EdgeHighlightmentSource edgeHighlightmentSource = EdgeHighlightmentSource.getHighlightmentSource( source );
 
-        perform( nodeHighlightmentSource, behaviourClass, session, in );
+        perform( edgeHighlightmentSource, behaviourClass, session, in );
     }
 }
