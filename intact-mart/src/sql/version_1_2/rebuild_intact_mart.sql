79115263c080160356c231ff895e697028d4e2b7
mmenden <mmenden@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-08-11 13:44:46 +0000

    create a rebuild script for the whole mart

---
+++ intact-mart/src/sql/version_1_2/rebuild_intact_mart.sql
@@ -0,0 +1,23 @@
+@ intact-mart/src/sql/version_1_2/1_0_drop_mart.sql
+@ intact-mart/src/sql/version_1_2/1_1_create_mart.sql
+--------------------------------------------------------------------------------
+@ intact-mart/src/sql/version_1_2/2_0_drop_views.sql
+@ intact-mart/src/sql/version_1_2/2_1_create_common_views.sql
+@ intact-mart/src/sql/version_1_2/2_2_create_publication_main_dm_views.sql
+@ intact-mart/src/sql/version_1_2/2_3_create_experiment_main_dm_views.sql
+@ intact-mart/src/sql/version_1_2/2_4_create_interaction_main_dm_views.sql
+@ intact-mart/src/sql/version_1_2/2_5_create_component_main_dm_views.sql
+@ intact-mart/src/sql/version_1_2/2_6_create_interactor_main_dm_views.sql
+@ intact-mart/src/sql/version_1_2/2_7_create_feature_main_dm_views.sql
+--------------------------------------------------------------------------------
+@ intact-mart/src/sql/version_1_2/3_0_insert_publication_main_dm.sql
+@ intact-mart/src/sql/version_1_2/3_1_insert_experiment_main_dm.sql
+@ intact-mart/src/sql/version_1_2/3_2_0_insert_tmp_molecule_count.sql
+@ intact-mart/src/sql/version_1_2/3_2_1_insert_interaction_main_dm.sql
+@ intact-mart/src/sql/version_1_2/3_3_insert_component_main_dm.sql
+@ intact-mart/src/sql/version_1_2/3_4_0_insert_tmp_sequence.sql
+@ intact-mart/src/sql/version_1_2/3_4_1_insert_interactor_main_dm.sql
+@ intact-mart/src/sql/version_1_2/3_5_insert_feature_main_dm.sql
+--------------------------------------------------------------------------------
+@ intact-mart/src/sql/version_1_2/1_2_grand_synonym_mart.sql
+
