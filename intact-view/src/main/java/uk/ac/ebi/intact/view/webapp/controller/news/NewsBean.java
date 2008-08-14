b70a5a339afc186a3658f6793fc389876c20e376
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-08-14 14:17:43 +0000

    integrate the news (news archive and news feed) into the web site

---
+++ intact-view/src/main/java/uk/ac/ebi/intact/view/webapp/controller/news/NewsBean.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2008 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.view.webapp.controller.news;
+
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import uk.ac.ebi.faces.component.news.FeedType;
+import uk.ac.ebi.faces.component.news.NewsUtil;
+import uk.ac.ebi.faces.model.news.News;
+import uk.ac.ebi.faces.model.news.NewsItem;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * News backing bean.
+ *
+ * @author Samuel Kerrien (skerrien@ebi.ac.uk)
+ * @version $Id$
+ */
+public class NewsBean implements Serializable {
+
+    public static final String NEWS_URL = "uk.ac.ebi.faces.NEWS_URL";
+
+    private News newsObject;
+    private List<NewsItem> news;
+    private List<NewsItem> urgentNews;
+
+    public NewsBean() {
+
+        String newsXml = FacesContext.getCurrentInstance().getExternalContext().getInitParameter( NEWS_URL );
+
+        newsObject = NewsUtil.readNews( newsXml );
+        news = newsObject.getNewsItem();
+
+        // urgent news
+        urgentNews = new ArrayList<NewsItem>();
+
+        for ( NewsItem newsItem : news ) {
+            if ( newsItem.isUrgent() != null && newsItem.isUrgent() ) {
+                urgentNews.add( newsItem );
+            }
+        }
+    }
+
+    public void exportFeed( ActionEvent evt ) {
+        SyndFeed feed = NewsUtil.createNewsFeed( newsObject );
+
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        try {
+            NewsUtil.writeFeed( feed, FeedType.DEFAULT, context );
+        }
+        catch ( FeedException e ) {
+            e.printStackTrace();
+        }
+        catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+        context.getApplication().getStateManager().saveSerializedView( context );
+        context.renderResponse();
+    }
+
+    public List<NewsItem> getNews() {
+        return news;
+    }
+
+    public void setNews( List<NewsItem> news ) {
+        this.news = news;
+    }
+
+    public News getNewsObject() {
+        return newsObject;
+    }
+
+    public void setNewsObject( News newsObject ) {
+        this.newsObject = newsObject;
+    }
+
+    public List<NewsItem> getUrgentNews() {
+        return urgentNews;
+    }
+
+    public void setUrgentNews( List<NewsItem> urgentNews ) {
+        this.urgentNews = urgentNews;
+    }
+}
