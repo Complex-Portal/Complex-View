677c89c3b33301aed0813c317a4922a560c3a6fa
mmenden <mmenden@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-08-21 10:45:02 +0000

    add short documentation

--- intact-mart/src/mart-build-sql/beta_1_0/rebuild_intact_mart.sql
+++ intact-mart/src/mart-build-sql/beta_1_0/rebuild_intact_mart.sql
@@ -1,6 +1,18 @@
+-- #############################################################################
+-- #   This is a rebuild script for an IntAct Oracle Mart                      #
+-- #############################################################################
+
+
+
+-- Drop and Create new IntAct Mart tables
 @ intact-mart/src/sql/version_1_2/1_0_drop_mart.sql
 @ intact-mart/src/sql/version_1_2/1_1_create_mart.sql
 --------------------------------------------------------------------------------
+
+
+
+-- Drop if existing all Materialized Views of IntAct Mart.
+-- Rebuild all necessary Views for the IntAct Mart.
 @ intact-mart/src/sql/version_1_2/2_0_drop_views.sql
 @ intact-mart/src/sql/version_1_2/2_1_create_common_views.sql
 @ intact-mart/src/sql/version_1_2/2_2_create_publication_main_dm_views.sql
@@ -10,14 +22,21 @@
 @ intact-mart/src/sql/version_1_2/2_6_create_interactor_main_dm_views.sql
 @ intact-mart/src/sql/version_1_2/2_7_create_feature_main_dm_views.sql
 --------------------------------------------------------------------------------
+
+
+
+-- Insert all values from the IntAct Model into the IntAct Mart
 @ intact-mart/src/sql/version_1_2/3_0_insert_publication_main_dm.sql
 @ intact-mart/src/sql/version_1_2/3_1_insert_experiment_main_dm.sql
-@ intact-mart/src/sql/version_1_2/3_2_0_insert_tmp_molecule_count.sql
+-- This is a specific cursor which calculates in a preprocessing
+-- step the molecule count of every interaction and store it in a
+-- temporary table.
+@ intact-mart/src/sql/version_1_2/3_2_0_insert_tmp_molecule_count.sql 
 @ intact-mart/src/sql/version_1_2/3_2_1_insert_interaction_main_dm.sql
 @ intact-mart/src/sql/version_1_2/3_3_insert_component_main_dm.sql
+-- This is again a specific cursor which concate in a preprocessing
+-- step the interactor sequence and store it also in a temporary 
+-- table.
 @ intact-mart/src/sql/version_1_2/3_4_0_insert_tmp_sequence.sql
 @ intact-mart/src/sql/version_1_2/3_4_1_insert_interactor_main_dm.sql
 @ intact-mart/src/sql/version_1_2/3_5_insert_feature_main_dm.sql
---------------------------------------------------------------------------------
-@ intact-mart/src/sql/version_1_2/4_0_grand_synonym_mart.sql
-
