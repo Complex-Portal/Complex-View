438d2beddcbac78e36e2fe5e474f7727be306fc8
skerrien <skerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2008-10-09 09:58:51 +0000

    Fix non compiling test due to changes in the psimitab indexer's constructor

--- intact-view/src/test/java/uk/ac/ebi/intact/view/webapp/util/OntologiesIndexWriterTest.java
+++ intact-view/src/test/java/uk/ac/ebi/intact/view/webapp/util/OntologiesIndexWriterTest.java
@@ -15,19 +15,19 @@
  */
 package uk.ac.ebi.intact.view.webapp.util;
 
-import org.junit.Test;
-import org.junit.Ignore;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.document.Document;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.obo.datamodel.OBOSession;
-import uk.ac.ebi.intact.dataexchange.cvutils.model.CvObjectOntologyBuilder;
-import uk.ac.ebi.intact.dataexchange.cvutils.OboUtils;
-import uk.ac.ebi.intact.dataexchange.cvutils.CvUpdater;
-import uk.ac.ebi.intact.psimitab.search.IntactPsimiTabIndexWriter;
 import uk.ac.ebi.intact.context.IntactContext;
+import uk.ac.ebi.intact.dataexchange.cvutils.CvUpdater;
+import uk.ac.ebi.intact.dataexchange.cvutils.OboUtils;
+import uk.ac.ebi.intact.dataexchange.cvutils.model.CvObjectOntologyBuilder;
+import uk.ac.ebi.intact.psimitab.search.IntactPsimiTabIndexWriter;
 
 import java.io.InputStream;
 
@@ -41,36 +41,36 @@ public class OntologiesIndexWriterTest {
 
     @Test
     @Ignore
-    public void case1() throws Exception{
-        InputStream file = OntologiesIndexWriterTest.class.getResourceAsStream("/psimitab/DGI-5424.tsv");
-        Directory binaryInteractionIndex = new RAMDirectory("/tmp");
+    public void case1() throws Exception {
+        InputStream file = OntologiesIndexWriterTest.class.getResourceAsStream( "/psimitab/DGI-5424.tsv" );
+        Directory binaryInteractionIndex = new RAMDirectory( "/tmp" );
 
-        IntactPsimiTabIndexWriter indexWriter = new IntactPsimiTabIndexWriter(true);
-        indexWriter.index(binaryInteractionIndex, file, true, true);
+        IntactPsimiTabIndexWriter indexWriter = new IntactPsimiTabIndexWriter();
+        indexWriter.index( binaryInteractionIndex, file, true, true );
 
         IntactContext.initStandaloneContextInMemory();
 
         OBOSession oboSession = OboUtils.createOBOSessionFromLatestMi();
 
-    	CvObjectOntologyBuilder cvBuilder = new CvObjectOntologyBuilder(oboSession);
+        CvObjectOntologyBuilder cvBuilder = new CvObjectOntologyBuilder( oboSession );
 
-    	CvUpdater cvUpdater = new CvUpdater();
-    	cvUpdater.createOrUpdateCVs(cvBuilder.getAllCvs());
+        CvUpdater cvUpdater = new CvUpdater();
+        cvUpdater.createOrUpdateCVs( cvBuilder.getAllCvs() );
 
         Directory newDirectory = new RAMDirectory();
 
         OntologiesIndexWriter writer = new OntologiesIndexWriter();
-        writer.createIndex(binaryInteractionIndex, newDirectory);
+        writer.createIndex( binaryInteractionIndex, newDirectory );
 
-        IndexSearcher searcher = new IndexSearcher(newDirectory);
+        IndexSearcher searcher = new IndexSearcher( newDirectory );
         IndexReader reader = searcher.getIndexReader();
 
-        for (int i=0; i<reader.maxDoc(); i++) {
-            Document doc = reader.document(i);
+        for ( int i = 0; i < reader.maxDoc(); i++ ) {
+            Document doc = reader.document( i );
 
-            System.out.println(doc.getField("identifier").stringValue()+" "+
-                               doc.getField("label").stringValue()+" - "+
-                               doc.getField("databaseLabel").stringValue());
+            System.out.println( doc.getField( "identifier" ).stringValue() + " " +
+                                doc.getField( "label" ).stringValue() + " - " +
+                                doc.getField( "databaseLabel" ).stringValue() );
         }
     }
 }
