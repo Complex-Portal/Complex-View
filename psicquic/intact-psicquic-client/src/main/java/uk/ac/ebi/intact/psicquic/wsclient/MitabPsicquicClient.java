e314a05656e0610961f75ed55de1bf459b394005
baranda <baranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2009-04-24 10:06:06 +0000

    Simplified project as most of the classes have been moved to the universal client

--- psicquic/intact-psicquic-client/src/main/java/uk/ac/ebi/intact/psicquic/wsclient/MitabPsicquicClient.java
+++ psicquic/intact-psicquic-client/src/main/java/uk/ac/ebi/intact/psicquic/wsclient/MitabPsicquicClient.java
@@ -15,16 +15,11 @@
  */
 package uk.ac.ebi.intact.psicquic.wsclient;
 
-import org.hupo.psi.mi.psicquic.*;
-import psidev.psi.mi.search.SearchResult;
+import org.hupo.psi.mi.psicquic.wsclient.AbstractMitabPsicquicClient;
 import psidev.psi.mi.tab.PsimiTabReader;
-import psidev.psi.mi.tab.model.BinaryInteraction;
 import uk.ac.ebi.intact.psimitab.IntactBinaryInteraction;
 import uk.ac.ebi.intact.psimitab.IntactPsimiTabReader;
 
-import java.util.List;
-import java.util.Collection;
-import java.util.ArrayList;
 
 /**
  * Client for a PSICQUIC Web service that returns PSIMITAB model objects.
@@ -32,105 +27,13 @@ import java.util.ArrayList;
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
  */
-public class MitabPsicquicClient extends AbstractPsicquicClient <SearchResult<IntactBinaryInteraction>> {
+public class MitabPsicquicClient extends AbstractMitabPsicquicClient<IntactBinaryInteraction> {
 
-    private static final String RETURN_TYPE = "psi-mi/tab25";
-
-    public MitabPsicquicClient(String serviceAddress) {
-        super(serviceAddress);
+    public MitabPsicquicClient(String s) {
+        super(s);
     }
 
-    public SearchResult<IntactBinaryInteraction> getByQuery(String query, int firstResult, int maxResults) throws PsicquicClientException {
-        RequestInfo requestInfo = createRequestInfo(RETURN_TYPE, firstResult, maxResults);
-
-        QueryResponse response;
-        try {
-            response = getService().getByQuery(query, requestInfo);
-        } catch (Exception e) {
-            throw new PsicquicClientException("There was a problem running the service", e);
-        }
-
-        return createSearchResult(response);
+    public PsimiTabReader newPsimiTabReader() {
+        return new IntactPsimiTabReader(false);
     }
-
-    public SearchResult<IntactBinaryInteraction> getByInteractor(String identifier, int firstResult, int maxResults) throws PsicquicClientException {
-        DbRef dbRef = createDbRef(identifier);
-        RequestInfo requestInfo = createRequestInfo(RETURN_TYPE, firstResult, maxResults);
-
-        QueryResponse response;
-        try {
-            response = getService().getByInteractor(dbRef, requestInfo);
-        } catch (Exception e) {
-            throw new PsicquicClientException("There was a problem running the service", e);
-        }
-
-        return createSearchResult(response);
-    }
-
-    public SearchResult<IntactBinaryInteraction> getByInteraction(String identifier, int firstResult, int maxResults) throws PsicquicClientException {
-        DbRef dbRef = createDbRef(identifier);
-        RequestInfo requestInfo = createRequestInfo(RETURN_TYPE, firstResult, maxResults);
-
-        QueryResponse response;
-        try {
-            response = getService().getByInteraction(dbRef, requestInfo);
-        } catch (Exception e) {
-            throw new PsicquicClientException("There was a problem running the service", e);
-        }
-
-        return createSearchResult(response);
-    }
-
-    public SearchResult<IntactBinaryInteraction> getByInteractionList(String[] identifiers, int firstResult, int maxResults) throws PsicquicClientException {
-        List<DbRef> dbRefs = createDbRefs(identifiers);
-        RequestInfo requestInfo = createRequestInfo(RETURN_TYPE, firstResult, maxResults);
-
-        QueryResponse response;
-        try {
-            response = getService().getByInteractionList(dbRefs, requestInfo);
-        } catch (Exception e) {
-            throw new PsicquicClientException("There was a problem running the service", e);
-        }
-
-        return createSearchResult(response);
-    }
-
-    public SearchResult<IntactBinaryInteraction> getByInteractorList(String[] identifiers, QueryOperand operand, int firstResult, int maxResults) throws PsicquicClientException {
-        List<DbRef> dbRefs = createDbRefs(identifiers);
-        RequestInfo requestInfo = createRequestInfo(RETURN_TYPE, firstResult, maxResults);
-
-        QueryResponse response;
-        try {
-            response = getService().getByInteractorList(dbRefs, requestInfo, operand.toString());
-        } catch (Exception e) {
-            throw new PsicquicClientException("There was a problem running the service", e);
-        }
-
-        return createSearchResult(response);
-    }
-
-    private SearchResult<IntactBinaryInteraction> createSearchResult(QueryResponse response) throws PsicquicClientException {
-        String mitab = response.getResultSet().getMitab();
-
-        PsimiTabReader reader = new IntactPsimiTabReader(true);
-        List<IntactBinaryInteraction> interactions = null;
-        try {
-            Collection<BinaryInteraction> interactionCollection = reader.read(mitab);
-            interactions = new ArrayList<IntactBinaryInteraction>();
-
-            for (BinaryInteraction binaryInteraction : interactionCollection) {
-                interactions.add((IntactBinaryInteraction)binaryInteraction);
-            }
-
-        } catch (Exception e) {
-            throw new PsicquicClientException("Problem converting the results to IntactBinaryInteractions", e);
-        }
-
-        ResultInfo resultInfo = response.getResultInfo();
-
-        return new SearchResult<IntactBinaryInteraction>(interactions, resultInfo.getTotalResults(),
-                                                                       resultInfo.getFirstResult(),
-                                                                       resultInfo.getBlockSize(), null);
-    }
-
 }
