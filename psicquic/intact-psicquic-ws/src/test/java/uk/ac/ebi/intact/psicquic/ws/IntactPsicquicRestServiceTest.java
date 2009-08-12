c51f1f4245390776954751acf2c79fb91e6ad306
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2009-08-12 10:25:46 +0000

    Fixed tests and added a new one to check on compressed REST streams

--- psicquic/intact-psicquic-ws/src/test/java/uk/ac/ebi/intact/psicquic/ws/IntactPsicquicRestServiceTest.java
+++ psicquic/intact-psicquic-ws/src/test/java/uk/ac/ebi/intact/psicquic/ws/IntactPsicquicRestServiceTest.java
@@ -15,21 +15,22 @@
  */
 package uk.ac.ebi.intact.psicquic.ws;
 
-import org.hupo.psi.mi.psicquic.PsicquicService;
-import org.junit.BeforeClass;
-import org.junit.Assert;
+import org.apache.cxf.jaxrs.impl.ResponseImpl;
 import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.apache.cxf.jaxrs.impl.ResponseImpl;
-import uk.ac.ebi.intact.dataexchange.psimi.solr.server.SolrJettyRunner;
-import uk.ac.ebi.intact.dataexchange.psimi.solr.IntactSolrIndexer;
 import uk.ac.ebi.intact.dataexchange.psimi.solr.CoreNames;
+import uk.ac.ebi.intact.dataexchange.psimi.solr.IntactSolrIndexer;
+import uk.ac.ebi.intact.dataexchange.psimi.solr.server.SolrJettyRunner;
 import uk.ac.ebi.intact.psicquic.ws.config.PsicquicConfig;
 import uk.ac.ebi.intact.psicquic.ws.util.PsicquicStreamingOutput;
 
-import java.io.InputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
 
 /**
  * @author Bruno Aranda (baranda@ebi.ac.uk)
@@ -57,7 +58,6 @@ public class IntactPsicquicRestServiceTest {
         IntactSolrIndexer indexer = new IntactSolrIndexer(solrJettyRunner.getSolrUrl( CoreNames.CORE_PUB ),
                                                           solrJettyRunner.getSolrUrl( CoreNames.CORE_ONTOLOGY_PUB ));
         final int lineCount = indexer.indexMitab( mitabStream, true );
-        System.out.println( "Line indexed: " + lineCount );
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"/META-INF/beans.spring.test.xml", "/META-INF/psicquic/jms.spring.xml"});
         PsicquicConfig config = (PsicquicConfig)context.getBean("testPsicquicConfig");
@@ -78,7 +78,7 @@ public class IntactPsicquicRestServiceTest {
 
     @Test
     public void testGetByQuery() throws Exception {
-        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "psi-mi/tab25", "0", "200");
+        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "tab25", "0", "200");
 
         PsicquicStreamingOutput pso = (PsicquicStreamingOutput) response.getEntity();
 
@@ -90,7 +90,7 @@ public class IntactPsicquicRestServiceTest {
 
     @Test
     public void testGetByQuery_maxResults() throws Exception {
-        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "psi-mi/tab25", "0", "3");
+        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "tab25", "0", "3");
 
         PsicquicStreamingOutput pso = (PsicquicStreamingOutput) response.getEntity();
 
@@ -98,5 +98,43 @@ public class IntactPsicquicRestServiceTest {
         pso.write(baos);
 
         Assert.assertEquals(3, baos.toString().split("\n").length);
+    }
+
+    @Test
+    public void testGetByQuery_maxResults_nolimit() throws Exception {
+        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "tab25", "0", String.valueOf(Integer.MAX_VALUE));
+
+        PsicquicStreamingOutput pso = (PsicquicStreamingOutput) response.getEntity();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        pso.write(baos);
+
+        Assert.assertEquals(11, baos.toString().split("\n").length);
+    }
+    
+    @Test
+    public void testGetByQuery_bin() throws Exception {
+        ResponseImpl response = (ResponseImpl) service.getByQuery("imatinib", "tab25-bin", "0", "200");
+
+        PsicquicStreamingOutput pso = (PsicquicStreamingOutput) response.getEntity();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        pso.write(baos);
+
+        // gunzip the output
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        GZIPInputStream gzipInputStream = new GZIPInputStream(bais);
+
+        ByteArrayOutputStream mitabOut = new ByteArrayOutputStream();
+
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = gzipInputStream.read(buf)) > 0)
+            mitabOut.write(buf, 0, len);
+
+        gzipInputStream.close();
+        mitabOut.close();
+
+        Assert.assertEquals(11, mitabOut.toString().split("\n").length);
     }
 }
