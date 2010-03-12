b5b2f43f92bebc617a7cb62b1d624b252fa71380
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-03-12 10:32:37 +0000

    Base controllers

---
+++ editor/src/main/java/uk/ac/ebi/intact/editor/controller/BaseController.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2010 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.editor.controller;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+/**
+ * IntAct JSF Base Controller.
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+public abstract class BaseController implements Serializable {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    protected void addMessage(String message, String detail) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(message, detail);
+        context.addMessage(null, facesMessage);
+    }
+
+    protected void addInfoMessage(String message, String detail) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, message, detail);
+        context.addMessage(null, facesMessage);
+    }
+
+    protected void addWarningMessage(String message, String detail) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_WARN, message, detail);
+        context.addMessage(null, facesMessage);
+    }
+
+    protected void addErrorMessage(String message, String detail) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, message, detail);
+        context.addMessage(null, facesMessage);
+    }
+}
+
+
