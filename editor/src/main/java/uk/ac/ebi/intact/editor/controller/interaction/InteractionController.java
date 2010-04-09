60b60f2deae927caf4d61ee4b8dd45e3a69f3b7f
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-04-09 16:21:55 +0000

    Basic display of the participants of an interaction

--- editor/src/main/java/uk/ac/ebi/intact/editor/controller/interaction/InteractionController.java
+++ editor/src/main/java/uk/ac/ebi/intact/editor/controller/interaction/InteractionController.java
@@ -15,7 +15,10 @@
  */
 package uk.ac.ebi.intact.editor.controller.interaction;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.myfaces.orchestra.conversation.annotations.ConversationName;
+import org.primefaces.model.DualListModel;
 import org.primefaces.model.LazyDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -26,8 +29,12 @@ import uk.ac.ebi.intact.editor.controller.publication.PublicationController;
 import uk.ac.ebi.intact.editor.controller.shared.AnnotatedObjectController;
 import uk.ac.ebi.intact.editor.util.LazyDataModelFactory;
 import uk.ac.ebi.intact.model.*;
+import uk.ac.ebi.intact.model.util.AnnotatedObjectUtils;
 
 import javax.faces.event.ComponentSystemEvent;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.*;
 
 /**
  * @author Bruno Aranda (baranda@ebi.ac.uk)
@@ -38,9 +45,15 @@ import javax.faces.event.ComponentSystemEvent;
 @ConversationName( "general" )
 public class InteractionController extends AnnotatedObjectController {
 
+    private static final Log log = LogFactory.getLog( InteractionController.class );
+
     private Interaction interaction;
     private String ac;
+    private DualListModel<String> experimentLists;
     private LazyDataModel<Component> participantDataModel;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private PublicationController publicationController;
@@ -56,20 +69,50 @@ public class InteractionController extends AnnotatedObjectController {
         return getInteraction();
     }
 
+    public DualListModel<String> getExperimentLists() {
+        return experimentLists;
+    }
+
+//    public Collection<Component> getComponent( int firstResult, int maxResults ) {
+//
+//        log.debug( "HqlLazyDataModel.fetchLazyData" );
+//        final String hqlQuery = "select count(c) from Component c where c.interaction.ac = '" + ac + "'";
+//        Query query = entityManager.createQuery( hqlQuery );
+//        log.debug( "HQL: " + hqlQuery );
+//
+//        query.setFirstResult( firstResult );
+//        query.setMaxResults( maxResults );
+//
+//        List<Component> results = (List<Component>) query.getResultList();
+//
+//        return results;
+//    }
+
     public void loadData( ComponentSystemEvent event ) {
         if ( ac != null ) {
             if ( interaction == null || !ac.equals( interaction.getAc() ) ) {
                 interaction = IntactContext.getCurrentInstance().getDaoFactory().getInteractionDao().getByAc( ac );
 
-                participantDataModel = LazyDataModelFactory.createLazyDataModel( getCoreEntityManager(),
-                                                                                 "select c from Component c where c.interaction.ac = '" + ac + "'",
-                                                                                 "select count(c) from Component c where c.interaction.ac = '" + ac + "'" );
+//                participantDataModel = LazyDataModelFactory.createLazyDataModel( getCoreEntityManager(),
+//                                                                                 "select c from Component c where c.interaction.ac = '" + ac + "'",
+//                                                                                 "select count(c) from Component c where c.interaction.ac = '" + ac + "'" );
             }
         } else {
             ac = interaction.getAc();
+
+//            if( participantDataModel == null ) {
+//                participantDataModel = LazyDataModelFactory.createLazyDataModel( getCoreEntityManager(),
+//                                                                                 "select c from Component c where c.interaction.ac = '" + ac + "'",
+//                                                                                 "select count(c) from Component c where c.interaction.ac = '" + ac + "'" );
+//            }
         }
 
-        // check if the publication or experiment are null in their controlles (this happens when the interaction
+        if( interaction.getExperiments().isEmpty() ) {
+            addWarningMessage( "This interaction isn't attached to an experiment", "Abort experiment loading." );
+            return;
+        }
+
+        // check if the publication or experiment are null in their controllers (this happens when the interaction
         // page is loaded directly using a URL)
         if ( publicationController.getPublication() == null ) {
             Publication publication = interaction.getExperiments().iterator().next().getPublication();
@@ -79,6 +122,28 @@ public class InteractionController extends AnnotatedObjectController {
         if ( experimentController.getExperiment() == null ) {
             experimentController.setExperiment( interaction.getExperiments().iterator().next() );
         }
+
+        // initialize the experiment lists
+        List<String> source  = new ArrayList<String>();
+        List<String> target  = new ArrayList<String>();
+
+
+        if(  interaction.getExperiments().size() > 1 ) {
+            addWarningMessage( "There are more than one experiment attached to this interaction", "" );
+        }
+
+        for ( Experiment e : interaction.getExperiments() ) {
+            source.add( e.getShortLabel() );
+        }
+
+        final Publication pub = publicationController.getPublication();
+        for ( Experiment e : pub.getExperiments() ) {
+            if( ! source.contains( e.getShortLabel() ) ) {
+                target.add( e.getShortLabel() );
+            }
+        }
+
+        experimentLists = new DualListModel<String>( source, target);
     }
 
     public String getAc() {
@@ -110,5 +175,27 @@ public class InteractionController extends AnnotatedObjectController {
 
     public LazyDataModel<Component> getParticipantDataModel() {
         return participantDataModel;
+    }
+
+    //////////////////////////////////
+    // Participant related methods
+
+    public String getInteractorIdentity(Interactor interactor) {
+        final Collection<InteractorXref> identities =
+                AnnotatedObjectUtils.searchXrefsByQualifier( interactor, CvXrefQualifier.IDENTITY_MI_REF );
+        StringBuilder sb = new StringBuilder(64);
+        for ( Iterator<InteractorXref> iterator = identities.iterator(); iterator.hasNext(); ) {
+            InteractorXref xref = iterator.next();
+            sb.append( xref.getPrimaryId() );
+            if( iterator.hasNext() ) {
+                sb.append( "|" );
+            }
+        }
+        return sb.toString();
+    }
+
+    public String getAuthorGivenName( Component component ) {
+        AnnotatedObjectUtils.getAliasByType( component, CvAliasType.AUTHOR_ASSIGNED_NAME_MI_REF );
+        return null;
     }
 }
