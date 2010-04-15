940c604bf8f210262b9b11e01a1970091a9f6afa
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-04-15 08:48:26 +0000

    Excluding gene name from the advanced fields because it does not seem to be working properly

--- intact-view/src/main/java/uk/ac/ebi/intact/view/webapp/controller/search/UserQuery.java
+++ intact-view/src/main/java/uk/ac/ebi/intact/view/webapp/controller/search/UserQuery.java
@@ -130,7 +130,7 @@ public class UserQuery extends BaseController {
                 new SearchField("", "All"),
                 new SearchField(FieldNames.IDENTIFIER, "Participant Id"),
                 new SearchField(FieldNames.INTERACTION_ID, "Interaction Id"),
-                new SearchField(FieldNames.GENE_NAME, "Gene name"),
+//                new SearchField(FieldNames.GENE_NAME, "Gene name"),
                 new SearchField(FieldNames.DETMETHOD, "Interaction detection method", "detectionMethodBrowser"),
                 new SearchField(FieldNames.TYPE, "Interaction type", "interactionTypeBrowser"),
                 new SearchField("species", "Organism", "taxonomyBrowser"),
