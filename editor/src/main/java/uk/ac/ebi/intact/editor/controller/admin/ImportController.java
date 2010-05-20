e74f111ee6968d82430e4ad20a6f4babb0ec06a3
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-05-20 16:56:14 +0000

    Button to create sample data

--- editor/src/main/java/uk/ac/ebi/intact/editor/controller/admin/ImportController.java
+++ editor/src/main/java/uk/ac/ebi/intact/editor/controller/admin/ImportController.java
@@ -3,17 +3,16 @@ package uk.ac.ebi.intact.editor.controller.admin;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.myfaces.orchestra.conversation.annotations.ConversationName;
-import org.primefaces.event.FileUploadEvent;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import uk.ac.ebi.intact.core.context.IntactContext;
 import uk.ac.ebi.intact.dataexchange.psimi.xml.exchange.PsiExchange;
 import uk.ac.ebi.intact.dataexchange.psimi.xml.exchange.PsiExchangeFactory;
+import uk.ac.ebi.intact.editor.controller.BaseController;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,18 +25,24 @@ import java.util.concurrent.TimeUnit;
 @Controller
 @Scope( "conversation.access" )
 @ConversationName( "admin" )
-public class ImportController {
+public class ImportController extends BaseController {
 
     private static final Log log = LogFactory.getLog( ImportController.class );
 
     private URL[] urlsToImport;
 
-//    public void handleFileUpload(FileUploadEvent event) {
-//        if (log.isInfoEnabled()) log.info("Uploaded: "+ event.getFile().getFileName());
-//
-//        FacesMessage msg = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
-//        FacesContext.getCurrentInstance().addMessage(null, msg);
-//    }
+    public void createSampleData(ActionEvent evt) {
+        CvManagement cvManagement = (CvManagement) IntactContext.getCurrentInstance().getSpringContext().getBean("cvManagement");
+        cvManagement.updateCvs();
+
+        try {
+            urlsToImport = new URL[] {new URL("ftp://ftp.ebi.ac.uk/pub/databases/intact/2010-05-07/psi25/pmid/2010/10037752.xml"),
+                           new URL("ftp://ftp.ebi.ac.uk/pub/databases/intact/2010-05-07/psi25/pmid/2010/10570129.xml")};
+            startImport(evt);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Problem importing data", e);
+        }
+    }
 
     public void startImport(ActionEvent evt) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
