a65a96c60c10b460d2355893495520942df1d5b0
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-12-02 12:31:40 +0000

    Improved notes system, so no prefix is necessary

---
+++ editor/src/main/java/uk/ac/ebi/intact/editor/controller/misc/MyNotesController.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright 2010 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.ebi.intact.editor.controller.misc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.intact.core.persister.IntactCore;
+import uk.ac.ebi.intact.core.users.model.Preference;
+import uk.ac.ebi.intact.core.users.model.User;
+import uk.ac.ebi.intact.editor.controller.JpaAwareController;
+import uk.ac.ebi.intact.editor.controller.UserSessionController;
+import uk.ac.ebi.intact.model.*;
+
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ComponentSystemEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id$
+ */
+@Controller
+@Scope("session")
+public class MyNotesController extends JpaAwareController {
+
+    private static final Log log = LogFactory.getLog(MyNotesController.class);
+
+    private String rawNotes;
+    private boolean editMode;
+
+    @Autowired
+    private UserSessionController userSessionController;
+
+    public MyNotesController() {
+    }
+
+    @Transactional("users")
+    public void loadPage(ComponentSystemEvent evt) {
+        User user = userSessionController.getCurrentUser();
+
+        Preference pref = user.getPreference("editor.notes");
+
+        if (pref == null) {
+            pref = new Preference(user, "editor.notes");
+            pref.setValue("This is an example of notes. \nYou can link to publications like EBI-2928483 or an interaction EBI-2928497.\n" +
+                    "You can use it as well for experiments, participants, etc.");
+
+            user.getPreferences().add(pref);
+
+            getUsersDaoFactory().getPreferenceDao().persist(pref);
+        }
+
+        rawNotes = pref.getValue();
+    }
+
+    @Transactional("users")
+    public void saveNotes(ActionEvent evt) {
+        User user = userSessionController.getCurrentUser();
+
+        Preference pref = user.getPreference("editor.notes");
+        pref.setValue(rawNotes);
+
+        getUsersDaoFactory().getPreferenceDao().update(pref);
+
+        editMode = false;
+    }
+
+    public String getFormattedNotes() {
+        String formatted = rawNotes.replaceAll("\n", "<br/>");
+
+        String acPrefix = getIntactContext().getConfig().getAcPrefix();
+
+        //Rewriter rewriter = new Rewriter("(\\w+):("+acPrefix+"-\\d+)", "<a href=\"/editor/{1}/{2}\">{2}</a>");
+        //formatted = rewriter.rewrite(formatted);
+
+        Pattern pattern = Pattern.compile(acPrefix+"-\\d+");
+        Matcher matcher = pattern.matcher(formatted);
+
+        StringBuffer sb = new StringBuffer(formatted.length()*2);
+
+        while (matcher.find()) {
+            String ac = matcher.group();
+
+            String replacement;
+
+            try {
+                Class aoClass = IntactCore.classForAc(getIntactContext(), ac);
+
+                String urlFolderName = null;
+
+                if (Publication.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "publication";
+                } else if (Experiment.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "experiment";
+                } else if (Interaction.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "interaction";
+                } else if (Interactor.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "interactor";
+                } else if (Component.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "participant";
+                } else if (Feature.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "feature";
+                } else if (BioSource.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "organism";
+                } else if (CvObject.class.isAssignableFrom(aoClass)) {
+                    urlFolderName = "cvobject";
+                }
+
+                replacement = "<a href=\"/editor/"+urlFolderName+"/"+ac+">"+ac+"</a>";
+
+            } catch (Throwable e) {
+                addWarningMessage("Accession problem", "Some accession numbers in the note could not be auto-linked because there is no object type for that accession");
+
+                replacement = ac;
+            }
+
+            matcher.appendReplacement(sb, replacement);
+        }
+
+        return sb.toString();
+    }
+
+    public String getRawNotes() {
+        return rawNotes;
+    }
+
+    public void setRawNotes(String rawNotes) {
+        this.rawNotes = rawNotes;
+    }
+
+    public boolean isEditMode() {
+        return editMode;
+    }
+
+    public void setEditMode(boolean editMode) {
+        this.editMode = editMode;
+    }
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("EBI-\\d+");
+
+        Matcher matcher = pattern.matcher("lalala EBI-1234 lololo EBI-32234, lili EBI-23123 jo");
+
+        StringBuffer sb = new StringBuffer();
+
+        while (matcher.find()) {
+            String ac = matcher.group();
+
+            matcher.appendReplacement(sb, "OOO"+ac+"oooo");
+        }
+
+        System.out.println(sb);
+    }
+}
