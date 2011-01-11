ca82259a7029b946e80672bc7f197f2e78019b1e
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2011-01-11 10:47:49 +0000

    Fixed an error in ValidatorContextUpdater

--- validator-view/src/main/java/uk/ac/ebi/intact/services/validator/jobs/ValidatorContextUpdater.java
+++ validator-view/src/main/java/uk/ac/ebi/intact/services/validator/jobs/ValidatorContextUpdater.java
@@ -70,7 +70,7 @@ public class ValidatorContextUpdater implements Job {
             // Both the ontology managers for PSI-MI and PSI-PAR are not up to date
             else if (!isPsiMiUpToDate && !isPsiParUpToDate){
                 // We will create a new validator web content which re-loads all the ontologies
-                newWebContent = new ValidatorWebContent();
+                newWebContent = new ValidatorWebContent(false);
             }
 
             // If the new Validator web content has been instantiated because one of the ontologies is not up-to-date
