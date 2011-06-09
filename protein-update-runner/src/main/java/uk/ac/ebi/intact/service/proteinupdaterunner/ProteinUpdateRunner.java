dcd6fe0a7a25f900405f090768c72994da0a5d3b
marine.dumousseau@wanadoo.fr <marine.dumousseau@wanadoo.fr@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2011-06-09 11:03:05 +0000

    Implemented the protein update runner

--- protein-update-runner/src/main/java/uk/ac/ebi/intact/service/proteinupdaterunner/ProteinUpdateRunner.java
+++ protein-update-runner/src/main/java/uk/ac/ebi/intact/service/proteinupdaterunner/ProteinUpdateRunner.java
@@ -15,8 +15,17 @@
  */
 package uk.ac.ebi.intact.service.proteinupdaterunner;
 
+import uk.ac.ebi.intact.core.context.IntactContext;
+import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateContext;
+import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateProcessor;
+import uk.ac.ebi.intact.dbupdate.prot.ProteinUpdateProcessorConfig;
+import uk.ac.ebi.intact.dbupdate.prot.report.FileReportHandler;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
- * TODO comment this class header.
+ * Main class for protein update runner
  *
  * @author Bruno Aranda (baranda@ebi.ac.uk)
  * @version $Id$
@@ -24,7 +33,47 @@ package uk.ac.ebi.intact.service.proteinupdaterunner;
 public class ProteinUpdateRunner {
 
     public static void main(String[] args) {
-        System.out.println("NOTHING");
+
+        // three possible arguments
+        if( args.length != 1 && args.length != 2 ) {
+            System.err.println( "Usage: GlobalUpdate <folder> <blast>" );
+            System.exit( 1 );
+        }
+        final String filename = args[0];
+
+        boolean isBlastEnabled = false;
+
+        if (args.length == 2){
+            isBlastEnabled = Boolean.parseBoolean(args[1]);
+        }
+
+        System.out.println( "folder where are the log files = " + filename );
+        System.out.println( "Blast enabled = " + isBlastEnabled );
+
+        IntactContext.initContext(new String[]{"/META-INF/jpa.spring.xml", "proteinupdaterunner.spring.xml"});
+
+        ProteinUpdateProcessorConfig config = ProteinUpdateContext.getInstance().getConfig();
+        config.setDeleteProteinTranscriptWithoutInteractions(true);
+        config.setDeleteProtsWithoutInteractions(true);
+        config.setGlobalProteinUpdate(true);
+        config.setFixDuplicates(true);
+        config.setProcessProteinNotFoundInUniprot(true);
+        config.setBlastEnabled(isBlastEnabled);
+        try {
+            config.setReportHandler(new FileReportHandler(new File(filename)));
+
+            ProteinUpdateProcessor updateProcessor = new ProteinUpdateProcessor();
+            System.out.println("Starting the global update");
+            updateProcessor.updateAll();
+            //List<Protein> proteins = updateProcessor.retrieveAndUpdateProteinFromUniprot("Q9XYZ4");
+            //List<String> acs = new ArrayList<String>();
+            //acs.add("EBI-3044019");
+            //updateProcessor.updateByACs(acs);
+
+        } catch (IOException e) {
+            System.err.println("The repository " + filename + " cannot be found. We cannot write log files and so we cannot run a global protein update.");
+            e.printStackTrace();
+        }
     }
 
 }
