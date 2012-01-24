27260c0f99ccc0ceedcf83a657e564f062dfff88
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-01-24 15:12:31 +0000

    Forgot to update one H2 port

--- editor/src/test/java/uk/ac/ebi/intact/editor/StartH2Server.java
+++ editor/src/test/java/uk/ac/ebi/intact/editor/StartH2Server.java
@@ -27,7 +27,7 @@ public class StartH2Server {
         System.out.println("////////////////////////////");
         System.out.println("Starting H2 database...");
 
-        final Server server = Server.createTcpServer("-tcpPort", "39101", "-baseDir", "target", "-tcpDaemon");
+        final Server server = Server.createTcpServer("-tcpPort", "39102", "-baseDir", "target", "-tcpDaemon");
         server.start();
 
         System.out.println("URL: "+server.getURL());
