7bc7018985662a65058512aae1cfdcee7c1b5c60
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-01-25 12:56:46 +0000

    Updated the script to rely on GlobalCvUpdateRunner

--- cv-update-runner/src/main/java/uk/ac/ebi/intact/service/CvUpdateRunner.java
+++ cv-update-runner/src/main/java/uk/ac/ebi/intact/service/CvUpdateRunner.java
@@ -1,7 +1,7 @@
 package uk.ac.ebi.intact.service;
 
 import uk.ac.ebi.intact.core.context.IntactContext;
-import uk.ac.ebi.intact.dbupdate.cv.CvUpdateManager;
+import uk.ac.ebi.intact.dbupdate.cv.GlobalCvUpdateRunner;
 
 /**
  * The global cv update runner
@@ -14,10 +14,10 @@ public class CvUpdateRunner
         IntactContext.initContext(new String[]{"/META-INF/jpa-cv-update.spring.xml", "/META-INF/cvupdaterunner.spring.xml",
                 "/META-INF/cvupdate.spring.xml"});
 
-        CvUpdateManager cm = (CvUpdateManager)
-                IntactContext.getCurrentInstance().getSpringContext().getBean("cvUpdateManager");
+        GlobalCvUpdateRunner cm = (GlobalCvUpdateRunner)
+                IntactContext.getCurrentInstance().getSpringContext().getBean("globalCvUpdateRunner");
 
-        System.out.println( "folder where are the log files = " + cm.getReportDirectory().getAbsolutePath() );
+        System.out.println( "folder where are the log files = " + cm.getCvUpdateManager().getReportDirectory().getAbsolutePath() );
 
         System.out.println("Starting the global update for PSI-MI and PSI-MOD");
         cm.updateAll();
