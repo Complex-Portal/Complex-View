f3bfde1fadf7e7a551448e40521e46ea50f62cfc
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2012-04-13 12:32:30 +0000

    added more config

--- imex-assigner-runner/assign-update-imex.sh
+++ imex-assigner-runner/assign-update-imex.sh
@@ -9,11 +9,9 @@ echo "Log directory $1"
 echo "IMEx central username $2"
 echo "IMEx central password $3"
 echo "IMEx central endpoint $4"
-echo "Path to the generated keystore $6"
-echo "Password of the generated keystore $7"
 
 MAVEN_PROFILE=$5
 
 echo "use profile ${MAVEN_PROFILE}"
 
-mvn -U clean install -Pexec,${MAVEN_PROFILE} -Dupdate.log.directory=$1 -Dic.username=$2 -Dic.password=$3 -Dic.endpoint=$4 -Dkeystore.path=$6 -Dkeystore.password=$7 -Dmaven.repo.local=repository -Dmaven.test.skip -Ddb=oracle
+mvn -U clean install -Pexec,${MAVEN_PROFILE} -Dupdate.log.directory=$1 -Dic.username=$2 -Dic.password=$3 -Dic.endpoint=$4 -Dmaven.repo.local=repository -Dmaven.test.skip -Ddb=oracle
