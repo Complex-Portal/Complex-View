1dead47649048a9390368b46c1f2f6245391e722
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2013-09-12 10:28:06 +0000

    Updated properties and created script

---
+++ complex-indexer/index_complexes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# == 1 ];
+then
+      MAVEN_PROFILE=$1
+      #MAVEN_OPTS="$MAVEN_OPTS -DmiFile=${INPUT_FILE} -DmiOutput=${OUTPUT_FILE}"
+      #export MAVEN_OPTS
+      echo "Profile: ${MAVEN_PROFILE}"
+      mvn clean install -P${MAVEN_PROFILE},index -Dspring.config=classpath:META-INF/complex-indexer-spring.xml -Dmaven.test.skip
+else
+      echo ""
+      echo "ERROR: wrong number of parameters ($#)."
+      echo "usage: MAVEN_PROFILE"
+      echo "usage: MAVEN_PROFILE: the name of the maven settings to use for connecting to the database."
+      echo ""
+      exit 1
+fi
