85f5c6d3eda3530e68c8d2a2c7cf67f1df7f2cff
oscar.forner.martinez@gmail.com <oscar.forner.martinez@gmail.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2013-11-13 16:08:39 +0000

    Complex service allow to return results in Json and Xml. Clean of old commented code

--- complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ComplexRestResult.java
+++ complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/ComplexRestResult.java
@@ -19,22 +19,10 @@ public class ComplexRestResult {
     private List<ComplexSearchResults> elements;
 
     public ComplexRestResult( ) { this.elements = new ArrayList<ComplexSearchResults>(); }
-    public ComplexRestResult( ComplexResultIterator iterator ) {
-        while ( iterator.hasNext() ) {
-            this.elements.add( iterator.next() ) ;
-        }
-    }
-
-    public void add( ComplexRestResult result ) { this.elements.addAll(result.elements); }
-    public void add( ComplexSearchResults result ) { this.elements.add(result); }
     public void add( ComplexResultIterator iterator ) {
         while ( iterator.hasNext() ) {
             this.elements.add( iterator.next() );
         }
-    }
-
-    public void setElements( Collection< ComplexSearchResults > elems ) {
-        this.elements = new ArrayList<ComplexSearchResults>(elems);
     }
     @XmlElement
     public List<ComplexSearchResults> getElements() { return elements; }
--- complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/SearchController.java
+++ complex-ws/src/main/java/uk/ac/ebi/intact/service/complex/SearchController.java
@@ -1,20 +1,10 @@
 package uk.ac.ebi.intact.service.complex;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
-import org.springframework.web.servlet.view.xml.MarshallingView;
 import uk.ac.ebi.intact.dataexchange.psimi.solr.complex.ComplexSolrSearcher;
 
-import javax.xml.bind.JAXBException;
-import javax.xml.stream.events.Comment;
-import java.util.Arrays;
 
 @Controller
 @RequestMapping("/search")
@@ -25,22 +15,8 @@ public class SearchController {
     private DataProvider dataProvider = new DataProvider() ;
 
     @RequestMapping(value = "/{query}",method = RequestMethod.GET)
-	public ComplexRestResult search(@PathVariable String query/*, @RequestParam (required = false) String format*/) {
-        //if (format == null) format = "json";
+	public ComplexRestResult search(@PathVariable String query) {
         return dataProvider.getData(query, this.searcher);
 	}
 
-   /* @Bean
-    public ViewResolver viewResolver() throws JAXBException {
-        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setClassesToBeBound(Comments.class, Comment.class);
-        MarshallingView marshallingView = new MarshallingView(jaxb2Marshaller);
-
-        ContentNegotiatingViewResolver contentNegotiatingViewResolver =
-                new ContentNegotiatingViewResolver();
-        contentNegotiatingViewResolver.setDefaultViews(Arrays.<View>
-                asList(new MappingJacksonJsonView(), marshallingView));
-        return contentNegotiatingViewResolver;
-    }
-   */
 }
