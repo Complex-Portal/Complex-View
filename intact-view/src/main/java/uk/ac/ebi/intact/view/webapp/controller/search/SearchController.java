58bff897f2a0085cb6414b395e2e15433c99d908
mdumousseau@yahoo.com <mdumousseau@yahoo.com@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2014-09-03 15:06:03 +0000

    Updated search panel

--- intact-view/src/main/java/uk/ac/ebi/intact/view/webapp/controller/search/SearchController.java
+++ intact-view/src/main/java/uk/ac/ebi/intact/view/webapp/controller/search/SearchController.java
@@ -168,12 +168,6 @@ public class SearchController extends JpaBaseController {
         return "/pages/interactions/interactions.xhtml?faces-redirect=true";
     }
 
-    public String doSearch() {
-        doBinarySearchAction();
-
-        return "/pages/interactions/interactions.xhtml";
-    }
-
     public String doBinarySearchWithInteractorFilterAction() {
         if (selectedInteractor != null){
             UserQuery userQuery = getUserQuery();
